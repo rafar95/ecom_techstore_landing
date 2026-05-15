d269c63f922f0b91e8762ec190a30ed5817ab41b
rafar95 <159924019+rafar95@users.noreply.github.com>
2026-05-15 10:22:23 +0200

    Initial commit

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+                    {label: 'Store' },
+                    {label: 'Mac' },
+                    {label: 'Watch' },
+                    {label: 'Vision' },
+                    {label: 'AirPods' },
+                ];
