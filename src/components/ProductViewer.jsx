3cca656d254c6e2655b5cd522589b4179e003703
rafar95 <159924019+rafar95@users.noreply.github.com>
2026-05-17 11:08:25 +0200

    Implement product section p2

--- src/components/ProductViewer.jsx
+++ src/components/ProductViewer.jsx
@@ -1,6 +1,9 @@
 import clsx from "clsx";
 import useMacbookStore from "../store";
 import { Canvas } from "@react-three/fiber";
+import { Box, OrbitControls } from "@react-three/drei";
+import MacbookModel14 from "./models/Macbook-14";
+import StudioLights from "./StudioLights";
 
 const ProductViewer = () => {
   const { color, scale, setColor, setScale } =useMacbookStore();
@@ -41,9 +44,13 @@ const ProductViewer = () => {
             </div>
         </div>
 
-        <canvas id="canvas">
+        <Canvas id="canvas" camera={{ position: [0, 2, 5], fov: 50, near: 0.1, far: 100}}>
+            <StudioLights />
 
-        </canvas>
+            <MacbookModel14 scale={0.06} position={[0, 0, 0]} />
+
+            <OrbitControls enableZoom={false} />
+        </Canvas>
     </section>
   )
 }
